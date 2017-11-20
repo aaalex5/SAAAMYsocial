fd1c2056fce0d98da37e62c69f46296a85f60e6c
Alex Alajbegovic <aaalex@alexs-macbook-pro.local>
2017-11-20 14:56:03 -0500

    Initial Commit

---
+++ socialmedia1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  socialmedia1
+//
+//  Created by Alex Alajbegovic on 11/20/17.
+//  Copyright © 2017 Alex Alajbegovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
